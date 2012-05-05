45420ede4120fb5fb24ec97afc4457f6a1362916
Will Benedict <wbenedict@squarespace.com>
2012-05-05 12:38:02 -0400

    Everything looks great!

--- dashboard/app/controllers/Application.java
+++ dashboard/app/controllers/Application.java
@@ -1,12 +1,15 @@
 package controllers;
 
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.index;
+
 
 public class Application extends Controller {
   
   public static Result index() {
-    return ok("Your new application is glorious!");
+    return ok(index.render("Your new application is ready."));
   }
   
 }
