70c6caaffc084bb6308e3339c4b0e824933890c1
Roger Kapsi <rkapsi@gmail.com>
2012-05-05 13:59:04 -0400

    Fix

--- dashboard/app/controllers/Application.java
+++ dashboard/app/controllers/Application.java
@@ -1,14 +1,16 @@
 package controllers;
 
-//import static org.ardverk.gibson.dashboard.Context.injector;
-
 import java.util.List;
 
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+//import static org.ardverk.gibson.dashboard.Context.injector;
+
+
 public class Application extends Controller {
+  
   private static GibsonService service = new MockService();
   
   public static Result index() {
