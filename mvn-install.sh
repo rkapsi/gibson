8bc358f4c304a6a04f63cf311f368e27913a744f
Roger Kapsi <rkapsi@gmail.com>
2012-06-06 06:20:24 -0400

    Little change to the scripts

--- mvn-install.sh
+++ mvn-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # http://issues.gradle.org/browse/GRADLE-2308
-gradle gibson-core:install
-gradle gibson-appender:install
+gradle gibson-core:install $*
+gradle gibson-appender:install $*
