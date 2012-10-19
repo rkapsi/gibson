de0a4eddb1b2fab3a6ad5d835e2a10ba6b2355c5
Doug Jones <doug@dfjones.net>
2012-10-18 23:01:46 -0400

    Use ConcurrentHashMap

--- gibson-dashboard/app/org/ardverk/gibson/dashboard/DefaultTrendService.java
+++ gibson-dashboard/app/org/ardverk/gibson/dashboard/DefaultTrendService.java
@@ -6,8 +6,9 @@ import org.ardverk.gibson.Event;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -18,8 +19,7 @@ public class DefaultTrendService implements TrendService {
   private static final long TREND_FREQUENCY = 15;
   private static final TimeUnit TREND_TIMEUNIT = TimeUnit.SECONDS;
 
-  private final Map<String, Callable<Long>> counterMap = new HashMap<String, Callable<Long>>();
-  private final Map<String, Trend> trendMap = new HashMap<String, Trend>();
+  private final Map<String, Pair<Trend, Callable<Long>>> trendMap = new ConcurrentHashMap<String, Pair<Trend, Callable<Long>>>();
 
   private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
@@ -57,54 +57,37 @@ public class DefaultTrendService implements TrendService {
   }
 
   @Override
-  public synchronized void clear() {
+  public void clear() {
     trendMap.clear();
-    counterMap.clear();
   }
 
   private Trend getTrendInfo(String key, Callable<Long> countGetter) {
-    Trend trend;
-    synchronized (this) {
-      trend = trendMap.get(key);
-    }
-    if (trend == null) {
+    Pair<Trend, Callable<Long>> pair = trendMap.get(key);
+    if (pair == null) {
       try {
         long count = countGetter.call();
-        trend = new Trend(count, count, count, System.currentTimeMillis());
-        synchronized (this) {
-          trendMap.put(key, trend);
-          counterMap.put(key, countGetter);
-        }
+        Trend trend = new Trend(count, count, count, System.currentTimeMillis());
+        pair = new ImmutablePair<Trend, Callable<Long>>(trend, countGetter);
+        trendMap.put(key, pair);
       } catch (Exception e) {
         // TODO better way to handle this?
         throw new RuntimeException(e);
       }
     }
 
-    return trend;
+    return pair.getLeft();
   }
 
   private void updateTrends() {
-    List<Pair<String, Trend>> trends = new ArrayList<Pair<String, Trend>>();
-    synchronized (this) {
-      for (Map.Entry<String, Trend> e : trendMap.entrySet()) {
-        trends.add(new ImmutablePair<String, Trend>(e.getKey(), e.getValue()));
-      }
-    }
-    for (Pair<String, Trend> p : trends) {
-      String key = p.getKey();
-      Trend trend = p.getValue();
+    for (Map.Entry<String, Pair<Trend, Callable<Long>>> e : trendMap.entrySet()) {
+      String key = e.getKey();
+      Trend trend = e.getValue().getLeft();
       try {
-        Callable<Long> counter;
-        synchronized (this) {
-          counter = counterMap.get(key);
-        }
+        Callable<Long> counter = e.getValue().getRight();
         if (counter != null) {
           long count = counter.call();
           Trend newTrend = Trend.create(count, trend);
-          synchronized (this) {
-            trendMap.put(key, newTrend);
-          }
+          trendMap.put(key, new ImmutablePair<Trend, Callable<Long>>(newTrend, counter));
         }
       } catch (Exception ex) {
         throw new RuntimeException(ex);
